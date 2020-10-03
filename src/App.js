909c74e93cf0285e56e7e7e2973d96ddf0176623
Canh <canhnt.btsoft@gmail.com>
2020-10-03 18:16:40 +0700

    Unknown commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div>
-      <h1>ReactApp</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>ReactApp</h1>
+        <Header />
+      </div>
+    );
+  }
 }
 
 export default App;

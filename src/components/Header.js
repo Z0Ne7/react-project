5cafc91c2a88838a3ed3f648c1235c2d999be620
Canh <canhnt.btsoft@gmail.com>
2020-10-05 15:18:21 +0700

    Finish lession 11

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 
-// class Header extends Component() {
-//   render() {
-//     return <h1>Header Component</h1>;
-//   }
-// }
-function Header() {
-  return <h1>Header</h1>;
+class Header extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-inverse">
+        <div className="container-fluid">
+          <a href="/" className="navbar-brand">
+            Component
+          </a>
+          <ul className="nav navbar-nav">
+            <li className="active">
+              <a href="/">Trang chủ</a>
+            </li>
+            <li>
+              <a href="/">Danh mục</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default Header;

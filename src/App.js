488d0dba8a81a4bc70536ab62a702ec8d97adfe1
Canh <canhnt.btsoft@gmail.com>
2020-10-06 09:57:50 +0700

    Done with form

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: '',
+      username: 'Nhập tên đăng nhập',
       password: '',
+      description: 'Nhập mô tả',
+      gender: 2,
+      language: 'vi',
+      status: true,
     };
     this.onHandleChange = this.onHandleChange.bind(this);
     this.onHandleSubmit = this.onHandleSubmit.bind(this);
@@ -14,7 +18,7 @@ class App extends Component {
   onHandleChange(event) {
     const target = event.target;
     const name = target.name;
-    const value = target.value;
+    const value = target.type === 'checkbox' ? target.checked : target.name;
     this.setState({
       [name]: value,
     });
@@ -43,6 +47,7 @@ class App extends Component {
                       className="form-control"
                       name="username"
                       onChange={this.onHandleChange}
+                      value={this.state.username}
                     />
                   </div>
                   <div className="form-group">
@@ -52,7 +57,64 @@ class App extends Component {
                       className="form-control"
                       name="password"
                       onChange={this.onHandleChange}
+                      value={this.state.password}
                     />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="">Mô tả</label>
+                    <textarea
+                      name="description"
+                      className="form-control"
+                      rows="3"
+                      onChange={this.onHandleChange}
+                      value={this.state.description}
+                    />
+                  </div>
+                  <select
+                    name="gender"
+                    className="form-control"
+                    value={this.state.gender}
+                    onChange={this.onHandleChange}
+                  >
+                    <option value={0}>Nữ</option>
+                    <option value={1}>Nam</option>
+                    <option value={2}>Khác</option>
+                  </select>
+                  <div className="form-group">
+                    <label htmlFor="">Ngôn ngữ</label>
+                    <div className="radio">
+                      <label>
+                        <input
+                          type="radio"
+                          name="language"
+                          value="vi"
+                          onChange={this.onHandleChange}
+                        />
+                        Tiếng Việt
+                      </label>
+                      <br />
+                      <label>
+                        <input
+                          type="radio"
+                          name="language"
+                          value="en"
+                          onChange={this.onHandleChange}
+                        />
+                        English
+                      </label>
+                    </div>
+                  </div>
+                  <div className="checkbox">
+                    <label>
+                      <input
+                        type="checkbox"
+                        value={true}
+                        name="status"
+                        onChange={this.onHandleChange}
+                        checked={this.state.status === true}
+                      />
+                      Trạng thái
+                    </label>
                   </div>
                   <button type="submit" className="btn btn-primary">
                     OK
